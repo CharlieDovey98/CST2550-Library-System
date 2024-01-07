b2e2cd625aeaa531aa8b03f627d1bd8204fbd5ba
Charlie Dovey <CharlieDovey98@hotmail.co.uk>
2024-01-07 15:05:01 +0000

    Added the Member object and implementation of functions to the Member and Person classes

--- member.cpp
+++ member.cpp
@@ -3,4 +3,23 @@
 
 // A derived class of the Person class. This class will inherit the members of the Person class.
 
+// Construct a member using the person class.
+Member::Member(int memberID, std::string name, std::string address, std::string email){
+    this->memberID = memberID;
+    this->setName(name);
+    this->setAddress(address);
+    this->setEmail(email);
+};
 
+std::string Member::getMemberID(){
+    return std::to_string(memberID);
+};
+
+std::vector<Book*> Member::getBooksBorrowed(){
+    return booksLoaned;
+};
+
+void Member::setBooksBorrowed(Book* book){
+    // user prompt, input and validation to be handled within the main.cpp file?
+    booksLoaned.push_back(book);
+};
--- person.cpp
+++ person.cpp
@@ -3,3 +3,26 @@
 
 // The base class. This class will be the parent class to librarian and member class.
 
+std::string Person::getName(){
+    return name;
+};
+
+void Person::setName(std::string &name){
+    this->name = name;
+};
+
+std::string Person::getAddress(){
+    return address;
+};
+
+void Person::setAddress(std::string &address){
+    this->address = address;
+};
+
+std::string Person::getEmail(){
+    return email;
+};
+
+void Person::setEmail(std::string& email){
+    this->email = email;
+};
