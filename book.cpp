a2a7616b9647e294b856d88cec5cfaa9b2f15abe
Charlie Dovey <CharlieDovey98@hotmail.co.uk>
2024-01-07 20:24:23 +0000

    Added the Member object and implementation of functions to the librarian and book classes

--- book.cpp
+++ book.cpp
@@ -1,4 +1,47 @@
 #include <iostream>
-#include "book.h" 
+#include "book.h"
 
+Book::Book(int bookID, std::string bookName, std::string authorFirstName, std::string authorLastName){
+    this->bookID = bookID;
+    this->bookName = bookName;
+    this->authorFirstName = authorFirstName;
+    this->authorLastName = authorLastName;
+};
 
+std::string Book::getBookID(){
+    return std::to_string(bookID);
+};
+
+std::string Book::getBookName(){
+    return bookName;
+};
+
+std::string Book::getAuthorFirstName(){
+    return authorFirstName;
+};
+
+std::string Book::getAuthorLastName(){
+    return authorLastName;
+};
+
+time_t Book::getDueDate(){
+    return dueDate;
+};
+
+void Book::setDueDate(time_t dueDate){
+    this->dueDate = dueDate;
+    // set the due date for 3 days from date of issue to the member.
+};
+
+void Book::returnBook(){
+// cout enter the bookID that is being returned: 
+// get due date
+// is it past the due date
+// calculateFine()
+// this book has been returned
+};
+
+void Book::borrowBook(Member *borrower, time_t dueDate){
+// setBooksBorrowed()
+// setDueDate()
+};
--- librarian.cpp
+++ librarian.cpp
@@ -3,3 +3,54 @@
 
 // A derived class of the Person class. This class will inherit the members of the Person class.
 
+Librarian::Librarian(int staffID, std::string name, std::string address, std::string email, int salary)
+{
+    this->staffID = staffID;
+    this->setName(name);
+    this->setAddress(address);
+    this->setEmail(email);
+    this->salary = salary;
+};
+
+void Librarian::addMember(){
+// cout enter them members name: 
+// cout enter the persons email:
+// cout enter the persons address: 
+// validation
+// create a memberID 
+// assign information to member
+// cout this person has been added as a member, here are their details:
+};
+
+void Librarian::issueBook(int memberID, int bookID){
+// bookID #### has been lent out to memberID ####
+};
+
+void Librarian::returnBook(int memberID, int bookID){
+// cout memberID #### has returned bookID ####
+};
+
+void Librarian::displayBorrowedBooks(int memberID){
+// cout here are the borrowed books by this member: 
+};
+
+void Librarian::calculateFine(int memberID){
+// days past due * $3
+// cout there is a fine with this return: $4.00
+};
+
+int Librarian::getStaffID(){
+    return staffID;
+};
+
+void Librarian::setStaffID(int staffID){
+    this->staffID = staffID;
+};
+
+int Librarian::getSalary(){
+    return salary;
+};
+
+void Librarian::setSalary(int salary){
+    this->salary = salary;
+};
