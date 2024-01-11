bdc0c5e0e000f8ee240671b87751e2622873492a
Charlie Dovey <CharlieDovey98@hotmail.co.uk>
2024-01-11 22:38:22 +0000

    Added comment, minor code changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -18,11 +18,13 @@ bool bookDataFileFound = false;
 
 void librarianPortal()
 {
-    std::cout << "Welcome librarian: " << Alexa.getName() << ", ID:" << Alexa.getStaffID() << "." << std::endl;
+    std::cout << "Librarian active: " << Alexa.getName() << ", ID:" << Alexa.getStaffID() << ".";
+    std::cout << "\n------------------------------------------" << std::endl;
 }
 
 int startMenu(int menuChoice)
 {
+    std::cout << "\n-------------------Menu-------------------";
     std::cout << "\nPlease choose from the following options:"
               << "\n    [1] Add a member."
               << "\n    [2] View a member's borrowed books."
@@ -33,12 +35,14 @@ int startMenu(int menuChoice)
     std::cout << "\nPlease enter your choice as a number: ";
     std::cin >> menuChoice;
 
-    // If the input is not an integer or is out of range get a new input from the user.
-    while (std::cin.fail() || menuChoice < 1 || menuChoice > 5) // Validation checks using a do while loop.
+    // A validation check using a while loop, guard statement.
+    while (std::cin.fail() || menuChoice < 1 || menuChoice > 5) // If the input is not an integer or is out of range get a new input from the user.
     {
         std::cout << "Invalid choice. Please enter a number between 1 and 5." << std::endl;
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "\nPlease enter your choice as a number: ";
+        std::cin >> menuChoice;
     }
     return menuChoice; // Once a correct input has been attained, return the input.
 }
@@ -48,23 +52,26 @@ void attainTheLibraryInformationFile()
     std::string fileName;
     std::ifstream inputFile;
 
-    std::cout << "Please enter the book data filename for the library management system (e.g. librarybooks.csv): ";
+    std::cout << "\nPlease enter the book data filename for the library management system (e.g. librarybooks.csv)" << std::endl;
     std::getline(std::cin, fileName);
 
-    // Check if the filename has .csv file extension.
-    while (fileName.size() <= 4 || fileName.substr(fileName.size() - 4) != ".csv")
+    // A validation check using a while loop, guard statement.
+    while (fileName.size() <= 4 || fileName.substr(fileName.size() - 4) != ".csv") // Check if the filename has .csv file extension.
     {
         std::cout << "Invalid file extension. Please ensure the file is a .csv file." << std::endl;
         std::cout << "Please enter the book data filename for the library management system (e.g. librarybooks.csv): ";
         std::getline(std::cin, fileName);
     }
-    inputFile.open(fileName);
-    while (!inputFile.is_open())
+    inputFile.open(fileName); // Attempt to open the file.
+
+    // A validation check using a while loop, guard statement.
+    while (!inputFile.is_open()) // If the file name specified cannot be opened, most likely due to the filename being incorrect or the file doesn't exist.
     {
-        std::cerr << "Failed to open file: " << fileName << std::endl;
-        std::cerr << "Please check if the file exists in the current directory." << std::endl;
+        std::cout << "Failed to open file: " << fileName << std::endl;
+        std::cout << "Please check if the file exists in the current directory." << std::endl;
         std::cout << "Please enter the book data filename for the library management system (e.g. librarybooks.csv): ";
         std::getline(std::cin, fileName);
+        inputFile.open(fileName);
     }
     bookDataFileFound = true;
 
@@ -72,9 +79,10 @@ void attainTheLibraryInformationFile()
     std::getline(inputFile, line); // Attain the first line from the document, the cell headings.
     line = "";                     // Discard the first headings line, as we only want to add books to the book vector.
 
-    while (std::getline(inputFile, line))
+    while (std::getline(inputFile, line)) // While there are still lines to be read in, within the .csv file, continue.
     {
         int bookID;
+        bool isIssued = false;
         std::string bookName;
         std::string pageCount;
         std::string authorFirstName;
@@ -93,7 +101,7 @@ void attainTheLibraryInformationFile()
         getline(inputString, bookType, ',');
 
         std::string line = "";
-        Book newBook(bookID, bookName, authorFirstName, authorLastName);
+        Book newBook(bookID, bookName, authorFirstName, authorLastName, isIssued);
         getBookList().push_back(newBook);
     }
     std::cout << "\nBooks added successfully\n";
@@ -102,22 +110,23 @@ void attainTheLibraryInformationFile()
 void printBooks()
 {
     const std::vector<Book> &books = getBookList(); // Use the getBookList() to access the vector of books.
-    for (const Book &book : books)                  // For loop through to print each book for testing purposes. This is not used in the main funcionality of the program.
+    for (const Book &book : books)                  // For loop through to print each book for testing purposes. 
+    // This is not used in the main funcionality of the program. This aids bug testing.
     {
         std::cout << "Book ID: " << book.getBookID() << ", "
                   << book.getBookName() << ", "
                   << book.getAuthorFirstName() << ", "
-                  << book.getAuthorLastName() << std::endl;
+                  << book.getAuthorLastName() << ", "
+                  << book.isBookIssued() << std::endl;
     }
 }
 
 int main(void)
 {
-    while (true)
+    while (true) // A while loop to loop on the menu. unneccessary? try to depricate before submission? ####
     {
 
         int menuChoice;
-
         std::cout << "\nLibrary Management System V1.\n"
                   << std::endl;
         librarianPortal();
@@ -127,7 +136,7 @@ int main(void)
             // A call the to the function to attain the library's book information.
             attainTheLibraryInformationFile();
             // A call to the function to print the now attained books the library system has in stock.
-            //printBooks();
+            printBooks();
         }
 
         menuChoice = startMenu(menuChoice);
@@ -144,7 +153,7 @@ int main(void)
             std::cout << "Please enter the member's ID to view their currently borrowed books: ";
             std::cin >> memberIDForBorrowedBooks;
 
-            // Validation checks using a while loop.
+            // A validation check using a while loop, guard statement.
             while (std::cin.fail() || Alexa.findMember(memberIDForBorrowedBooks) == nullptr) // If memberID doesn't exist or std::cin fails.
             {
                 std::cout << "Invalid memberID. Please enter the memberID in numbers, of a registered member." << std::endl;
@@ -164,7 +173,7 @@ int main(void)
             std::cout << "Please enter the memberID of the member that wants to borrow a book: ";
             std::cin >> memberIDToIssueABook;
 
-            // Validation checks using a while loop.
+            // A validation check using a while loop, guard statement.
             while (std::cin.fail() || Alexa.findMember(memberIDToIssueABook) == nullptr) // If memberID doesn't exist or std::cin fails.
             {
                 std::cout << "Invalid memberID. Please enter the memberID in numbers, of a registered member." << std::endl;
@@ -178,7 +187,7 @@ int main(void)
             std::cout << "Please enter the bookID of the book that is being issued out: ";
             std::cin >> bookIDToIssueABook;
 
-            // Validation checks using a while loop.
+            // A validation check using a while loop, guard statement.
             while (std::cin.fail() || Alexa.findBook(bookIDToIssueABook) == nullptr) // If bookID doesn't exist or std::cin fails.
             {
                 std::cout << "Invalid bookID. Please enter the bookID in numbers, of a in stock library book." << std::endl;
@@ -204,7 +213,7 @@ int main(void)
             std::cout << "Please enter the memberID of the member that wants to return a book: ";
             std::cin >> memberIDToIssueABook;
 
-            // Validation checks using a while loop.
+            // A validation check using a while loop, guard statement.
             while (std::cin.fail() || Alexa.findMember(memberIDToIssueABook) == nullptr) // If memberID doesn't exist or std::cin fails.
             {
                 std::cout << "Invalid memberID. Please enter the memberID in numbers, of a registered member." << std::endl;
@@ -218,7 +227,7 @@ int main(void)
             std::cout << "Please enter the bookID of the book that is being returned: ";
             std::cin >> bookIDToIssueABook;
 
-            // Validation checks using a while loop.
+            // A validation check using a while loop, guard statement.
             while (std::cin.fail() || Alexa.findBook(bookIDToIssueABook) == nullptr) // If bookID doesn't exist or std::cin fails.
             {
                 std::cout << "Invalid bookID. Please enter the bookID in numbers, of a in stock library book." << std::endl;
